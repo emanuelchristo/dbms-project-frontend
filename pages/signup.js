c7cbb1d48d50133d89469d42466b153575bbc3cc
abhi_ram <abhiramjayapuri2015@gmail.com>
2022-11-23 02:39:33 +0530

    added signup page

---
+++ pages/signup.js
@@ -0,0 +1,30 @@
+import styles from 'components/signup/signup-page.module.css'
+
+export default function SigninPage() {
+	return (
+		<div className={styles['container']}>
+			<img className={styles['logo']} src='logo.svg' />
+			<h1 className={styles['heading']}>
+				Create a new <br />
+				account
+			</h1>
+
+			<div className={styles['form-wrapper']}>
+				<input type='text' placeholder='Name' />
+				<input type='email' placeholder='Email' />
+				<input type='password' placeholder='Password' />
+				<button className={styles['button']}>
+					<span>Create account</span> <img src='svgs/right-long-arrow-black.svg' />
+				</button>
+				<div className={styles['or-container']}>
+					<div className={styles['or-line']}></div>
+					<span>or</span>
+					<div className={styles['or-line']}></div>
+				</div>
+				<button className={`${styles['button']} ${styles['grey']}`}>
+					<span>Sign in</span> <img src='svgs/right-long-arrow-black.svg' />
+				</button>
+			</div>
+		</div>
+	)
+}
